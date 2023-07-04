41d8f13cc8db24ee347ce94b6434f82eaa3f9f01
Joaquin Blanco <blancojoaquin77@gmail.com>
2023-07-04 00:27:39 -0300

    vulnerabilities fixed

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 // jest.config.js
 const nextJest = require("next/jest");
-const { pathsToModuleNameMapper } = require("ts-jest");
-const { compilerOptions } = require("./tsconfig.json");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
@@ -16,7 +14,11 @@ const customJestConfig = {
   // if using TypeScript with a baseUrl set to the root directory then you need the below for alias' to work
   moduleDirectories: ["node_modules", "<rootDir>"],
   testEnvironment: "jest-environment-jsdom",
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  moduleNameMapper: {
+    "^@/src/(.*)": "<rootDir>/src/$1",
+    "^@/pages/(.*)": "<rootDir>/pages/$1",
+    "^@/test/(.*)": "<rootDir>/test/$1",
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
